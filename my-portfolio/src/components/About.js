b7a05197d61c8a83184874d73dc797f1589e0c07
Tyler <tyler.dins88@gmail.com>
2023-05-08 17:21:20 -0700

    fixed spelling

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -19,7 +19,7 @@ function About() {
                     <Card.Text>
                         <p>I graduated from the University of Nebraska at Lincoln in 2011 with a double degree in Finance and Economics. Over the next decade I worked a variety of opportunities that expanded my network, bettered my technical skills, and learned many new ones.</p>
 
-                        <p>In the hopsitality industry I worked with people on vacation, homeowners, HOA's, and local management. I developed several important skills such as strong communication skills, patience & empathy, conflict resolution, teamwork, time management, and attention to detail. You must be professional at all times and know how to commuicate effectively with everyone.  </p>
+                        <p>In the hopsitality industry I worked with people on vacation, homeowners, HOA's, and local management. I developed several important skills such as strong communication skills, patience & empathy, conflict resolution, teamwork, time management, and attention to detail. You must be professional at all times and know how to communicate effectively with everyone.  </p>
 
                         <p>The sales industry is all about connecting a consumer with the desired product or service they want. You learn to how to show the value of what you offer, negotiate, and problem-solve. Skills learned here include product knowledge, sales techniques & strategies, goal-oriented mindset, persistence and resilience. This creates and maintains a loyal relationship between the consumer and provider.</p>
                     </Card.Text>
