379dd4b662067de448485d50cbda166be087f99f
Tyler <tyler.dins88@gmail.com>
2023-06-28 22:38:35 -0700

    connect contact form

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 
 function BasicExample() {
     return (
-        <Form id="form" style={{ width: '60vw' }}>
+        <Form target="_blank" action="https://formsubmit.co/22f6a854605d1d7f65de5591cb289db4" method="POST" id="form" style={{ width: '60vw' }}>
             <Form.Group className="mb-3">
                 <Form.Label>name:</Form.Label>
                 <Form.Control type="name" placeholder="enter name here." />
@@ -26,3 +26,23 @@ function BasicExample() {
 }
 
 export default BasicExample;
+
+{/* <div class="container">
+  <h1>FormSubmit Demo</h1>
+  <form target="_blank" action="https://formsubmit.co/tyler.dins88@gmail.com" method="POST">
+    <div class="form-group">
+      <div class="form-row">
+        <div class="col">
+          <input type="text" name="name" class="form-control" placeholder="Full Name" required>
+        </div>
+        <div class="col">
+          <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
+  </form>
+</div> */}
