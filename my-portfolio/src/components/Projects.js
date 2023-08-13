6df2e62b43d7e26bcfe086de0a2955db0f07a425
Tyler <tyler.dins88@gmail.com>
2023-08-13 12:16:04 -0700

    update project page

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -12,8 +12,8 @@ import "../index.css";
 function Projects() {
     return (
         <Row className='projects'>
-            <Card className="needle" style={{ width: '19rem' }}>
-                <Card.Img variant="top" src={Needle} />
+            <Card className="needle card-hover" style={{ width: '19rem' }}>
+                <Card.Img variant="top" className="projectImg" src={Needle} />
                 <Card.Body>
                     <Card.Title>Needle / Junkeez</Card.Title>
                     <Card.Text>
@@ -23,8 +23,8 @@ function Projects() {
                     <Button variant="primary" target="_blank" rel="noreferrer">coming soon.</Button>
                 </Card.Body>
             </Card>
-            <Card className="aquadown" style={{ width: '19rem' }}>
-                <Card.Img variant="top" src={Aqua} />
+            <Card className="aquadown card-hover" style={{ width: '19rem' }}>
+                <Card.Img variant="top" className="projectImg" src={Aqua} />
                 <Card.Body>
                     <Card.Title>aquaDown</Card.Title>
                     <Card.Text>
@@ -34,8 +34,8 @@ function Projects() {
                     <Button variant="primary" target="_blank" rel="noreferrer" href="https://aquadown.herokuapp.com/">deployed link.</Button>
                 </Card.Body>
             </Card>
-            <Card className='boozebed' style={{ width: '19rem' }}>
-                <Card.Img variant="top" src={Booze} />
+            <Card className='boozebed card-hover' style={{ width: '19rem' }}>
+                <Card.Img variant="top" className="projectImg" src={Booze} />
                 <Card.Body>
                     <Card.Title>Booze & Bed</Card.Title>
                     <Card.Text>
@@ -45,8 +45,8 @@ function Projects() {
                     <Button variant="primary" target="_blank" rel="noreferrer" href="https://tylerdins88.github.io/traveler-project/">deployed link.</Button>
                 </Card.Body>
             </Card>
-            <Card className='weatherdash' style={{ width: '19rem' }}>
-                <Card.Img variant="top" src={Weather} />
+            <Card className='weatherdash card-hover' style={{ width: '19rem' }}>
+                <Card.Img variant="top" className="projectImg" src={Weather} />
                 <Card.Body>
                     <Card.Title>weather dashboard</Card.Title>
                     <Card.Text>
@@ -56,8 +56,8 @@ function Projects() {
                     <Button variant="primary" target="_blank" rel="noreferrer" href="https://tylerdins88.github.io/weather-dashboard/">deployed link.</Button>
                 </Card.Body>
             </Card>
-            <Card className='firstport' style={{ width: '19rem' }}>
-                <Card.Img variant="top" src={First} />
+            <Card className='firstport card-hover' style={{ width: '19rem' }}>
+                <Card.Img variant="top" className="projectImg" src={First} />
                 <Card.Body>
                     <Card.Title>first portfolio</Card.Title>
                     <Card.Text>
@@ -67,8 +67,8 @@ function Projects() {
                     <Button variant="primary" target="_blank" rel="noreferrer" href="https://tylerdins88.github.io/tyler-dins-portfolio/">deployed link.</Button>
                 </Card.Body>
             </Card>
-            <Card className='favmus' style={{ width: '19rem' }}>
-                <Card.Img variant="top" src={Fav} />
+            <Card className='favmus card-hover' style={{ width: '19rem' }}>
+                <Card.Img variant="top" className="projectImg" src={Fav} />
                 <Card.Body>
                     <Card.Title>favorite musician</Card.Title>
                     <Card.Text>
